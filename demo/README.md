4eac804c11e37c5bfb19d12469c417a9049038d5
NathanGRomano <nathan.g.romano@gmail.com>
2015-08-30 00:22:22 -0400

    whipped up a simple near real time demo using express, socket.io, and jquery.

---
+++ demo/README.md
@@ -0,0 +1,12 @@
+#Game of Life Demo
+
+To get things running
+
+```
+git clone git@github.com:NathanGRomano/game-of-life.git
+cd game-of-life/demo
+npm install
+PORT=3000 node server.js
+```
+
+Go to your browser and open up `http://localhost:3000/`.
---
+++ demo/server.js
@@ -0,0 +1,40 @@
+var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname+'/public'));
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var Grid = require('../lib').Grid;
+var screen = {
+  grid: function () {
+    if (!this._grid) this._grid = Grid(100,100);
+    return this._grid;
+  },
+  live: function (x,y) { 
+    this.grid().live(x,y);
+    return this;
+  },
+  die: function (x,y) {
+    this.grid().die(x,y);
+    return this;
+  },
+  tick: function () {
+    this._grid = this.grid().generate();
+    return this;
+  }
+};
+var grid = Grid(100,100);
+setInterval(function () {
+  io.emit('cells', screen.tick().grid().cells);
+}, 1000);
+io.on('connection', function (sock) {
+  sock.on('live', function (x, y) {
+    console.log('live %s,%s',x,y);
+    screen.live(x, y);
+  });
+  sock.on('die', function (x, y) {
+    console.log('die %s,%s',x,y);
+    screen.die(x, y);
+  });
+});
+server.listen(port);
